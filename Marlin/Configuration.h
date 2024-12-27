45e8da9ea256343eb97e4439b9efb5b59eafaa3e
Chase Thompson-Baugh <dr_modean@me.com>
2024-12-27 16:27:29 -0700

    Refine offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1637,7 +1637,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -57, -13, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -56.2, -11.6, 0 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
